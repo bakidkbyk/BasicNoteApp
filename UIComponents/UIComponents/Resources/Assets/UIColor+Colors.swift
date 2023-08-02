a6ed063fbc1e718e08bd4d6971cab2f84e2ff8c1
Ümit Baki Dikbıyık <umitbaki.dkbyk@gmail.com>
2023-08-02 16:04:04 +0300

    add colors

--- UIComponents/UIComponents/Resources/Assets/UIColor+Colors.swift
+++ UIComponents/UIComponents/Resources/Assets/UIColor+Colors.swift
@@ -14,6 +14,14 @@ public extension UIColor {
       return Asset.Colors.actionSecondary.color
   }
 
+  static var textPrimary: UIColor {
+      return Asset.Colors.textPrimary.color
+  }
+
+  static var textSecondary: UIColor {
+      return Asset.Colors.textSecondary.color
+  }
+
   static var appBorderColor: UIColor {
       return Asset.Colors.appBorderColor.color
   }
@@ -28,14 +36,6 @@ public extension UIColor {
 
   static var appYellow: UIColor {
       return Asset.Colors.appYellow.color
-  }
-
-  static var textPrimary: UIColor {
-      return Asset.Colors.textPrimary.color
-  }
-
-  static var textSecondary: UIColor {
-      return Asset.Colors.textSecondary.color
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
--- UIComponents/UIComponents/Resources/Assets/UIImage+Images.swift
+++ UIComponents/UIComponents/Resources/Assets/UIImage+Images.swift
@@ -5,6 +5,14 @@ import UIKit
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public extension UIImage {
+
+  static var icError: UIImage {
+      return Asset.Images.icError.image
+  }
+
+  static var imgUser: UIImage {
+      return Asset.Images.imgUser.image
+  }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
